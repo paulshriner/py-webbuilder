6d5b1b2acc42a7af7ed319aecb7f9a8555e5a27a
paulshriner <shrinerpaul@gmail.com>
2026-02-17 19:13:30 -0500

    use insert with index in gen to plan for nesting

--- src/gen.py
+++ src/gen.py
@@ -13,43 +13,49 @@ def generate_html(root_path: str, temp_file_path: str) -> None:
     temp_file = open(temp_file_path, 'r')
     final_file = open(f"temp/{file_name}.final", 'w')
     html_line = []
-    ending_tags = []
+    index = 0
     for line in temp_file:
         # Need this as each line has a newline at the end, we don't want this
         line_stripped = line[0:-1]
 
         match line_stripped:
             case "<HEADING_1>":
-                html_line.append("<h1>")
-                ending_tags.append("</h1>")
+                html_line.insert(index, "<h1>")
+                index += 1
+                html_line.insert(index, "</h1>")
             case "<HEADING_2>":
-                html_line.append("<h2>")
-                ending_tags.append("</h2>") 
+                html_line.insert(index, "<h2>")
+                index += 1
+                html_line.insert(index, "</h2>")
             case "<HEADING_3>":
-                html_line.append("<h3>")
-                ending_tags.append("</h3>") 
+                html_line.insert(index, "<h3>")
+                index += 1
+                html_line.insert(index, "</h3>")
             case "<HEADING_4>":
-                html_line.append("<h4>")
-                ending_tags.append("</h4>") 
+                html_line.insert(index, "<h4>")
+                index += 1
+                html_line.insert(index, "</h4>")
             case "<HEADING_5>":
-                html_line.append("<h5>")
-                ending_tags.append("</h5>") 
+                html_line.insert(index, "<h5>")
+                index += 1
+                html_line.insert(index, "</h5>")
             case "<HEADING_6>":
-                html_line.append("<h6>")
-                ending_tags.append("</h6>")
+                html_line.insert(index, "<h6>")
+                index += 1
+                html_line.insert(index, "</h6>")
             case "<TEXT>":
-                html_line.append("<p>")
-                ending_tags.append("</p>")
+                html_line.insert(index, "<p>")
+                index += 1
+                html_line.insert(index, "</p>")
             case "<NEW_LINE>":
-                for i in ending_tags[::-1]:
-                    html_line.append(i)
                 final_file.write("".join(html_line) + "\n")
                 html_line = []
-                ending_tags = []                
+                index = 0               
             case "<EMPTY_LINE>":
                 final_file.write("<br>" + "\n")              
             case default:
-                html_line.append(line_stripped)
+                html_line.insert(index, line_stripped)
+                index += 1
 
     # Close files when done
     temp_file.close()
